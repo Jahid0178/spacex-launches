72574b84f8e614bbc4dee3e80a0d7de9c9b10630
Zahid <zahidul.haque767@gmail.com>
2023-09-24 03:40:11 +0600

    feat: added home page content
    feat: header component make sticky
    
    feat: creating home section reusable component
    
    feat: added few global css
    
    feat: added home page content data in data file

---
+++ app/(client)/information/page.tsx
@@ -0,0 +1,14 @@
+import Title from "@/components/Title/Title";
+import React from "react";
+
+const InformationPage = () => {
+  return (
+    <section className="bg-primary text-primary section">
+      <div className="container">
+        <Title title="Informations" />
+      </div>
+    </section>
+  );
+};
+
+export default InformationPage;
